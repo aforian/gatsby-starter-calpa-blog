03a491034cc4a0c0955b7c932f9cef1988f68272
Alex Ian <alexian853@gmail.com>
2023-04-22 15:34:52 +0800

    feat: try `gatsby-plugin-google-gtag` plugin

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,20 +1,10 @@
-/* eslint-disable import/prefer-default-export */
-import ReactGA from 'react-ga';
 import { config } from './data';
 
-const {
-  url, gaTrackId, gaOptimizeId,
-} = config;
+const { url } = config;
 
 const isLocalDevelopment = () => window && window.location && window.location.origin !== url;
 
 if (isLocalDevelopment() === false) {
-  ReactGA.initialize(gaTrackId);
-
-  // Google Optimizer
-  if (gaOptimizeId) {
-    ReactGA.ga('require', gaOptimizeId);
-  }
   console.log('Welcome to online environment.');
 }
 
@@ -24,11 +14,3 @@ console.log(
   'color: #6cf; background: #030307; padding:5px 0;',
   'background: #6cf; padding:5px 0;',
 );
-
-export const onRouteUpdate = state => {
-  if (isLocalDevelopment() !== true) {
-    ReactGA.pageview(state.location.pathname);
-  } else {
-    console.log('isLocalDevelopment is true, so ReactGA is not activated');
-  }
-};
