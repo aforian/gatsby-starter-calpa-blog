09b39c83a98b4a58c158e33cae3b0e563e5c5a45
Alex Ian <alexian853@gmail.com>
2023-04-23 18:26:18 +0800

    Feature/markdown linked file (#53)
    * feat: add `gatsby-remark-copy-linked-files` package
    
    * feat: handle image without sharp

--- src/templates/tag.js
+++ src/templates/tag.js
@@ -71,6 +71,7 @@ export const pageQuery = graphql`
             date
             tags
             headerImage {
+              publicURL
               childImageSharp {
                 gatsbyImageData(
                   width: 300
